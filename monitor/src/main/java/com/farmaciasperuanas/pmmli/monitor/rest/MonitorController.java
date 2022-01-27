b45fd62e6cd94de69aa7f059e70799af69356084
Carlos Zarate <zaratec31@hotmail.com>
2022-01-27 18:44:06 -0500

    Agregando cambios2

--- monitor/src/main/java/com/farmaciasperuanas/pmmli/monitor/rest/MonitorController.java
+++ monitor/src/main/java/com/farmaciasperuanas/pmmli/monitor/rest/MonitorController.java
@@ -122,6 +122,8 @@ public class MonitorController {
         entity.setTaskState("I");
         entity.setCronExpression(null);
         transactionTaskService.save(entity);
+
+        
       }
     }catch (Exception e)
     {
